f1ae4c710adf080078edad768992ea9ed8e3fc8e
jrodas2021418 <107076089+jrodas2021418@users.noreply.github.com>
2023-07-22 22:21:46 -0600

    Add files via upload

---
+++ AdoptPets_BackEnd/src/routes/mascotas.routes.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const {Router} = require('express');
+const { addPet, listPet, editPet, deletePet } = require('../controllers/mascota.controller');
+
+const api = Router();
+
+api.post('/agregar-pet', addPet)
+api.get('/listar-pet', listPet)
+api.put('/edit-pet/:id', editPet)
+api.delete('/delete-pet/:id', deletePet)
+
+module.exports = api;
