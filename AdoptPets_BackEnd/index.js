82b7f94af6eeb761644d10ce416b9685e36546f0
Cristian Suar <csuar-2017085@kinal.edu.gt>
2023-07-24 01:06:06 -0600

    Se agrego crud de mascotas

--- AdoptPets_BackEnd/index.js
+++ AdoptPets_BackEnd/index.js
@@ -5,13 +5,14 @@ const app =express();
 const {connection}=require("./src/database/connection");
 require('dotenv').config();
 const port = process.env.PORT;
-
+const Mascota = require('./src/routes/mascotas.routes');
 
 connection();
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
 
+app.use('/api', Mascota);
 
 app.listen(port, () =>{
     console.log(`El servidor esta corriendo el puerto ${port}`);
